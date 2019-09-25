643c36f1eacee1618471f7dc598462715a28f70d
Parth Dixit <parthd@codeaurora.org>
2019-09-25 16:41:42 +0530

    QcomModulePkg: Fastboot: add a check in meta image
    Do not allow flashing of critical partitions while flashing images
    in meta image format.
    
    Change-Id: I9c6e790b8df0acf003dcf81692272b19791d696b

--- QcomModulePkg/Library/FastbootLib/FastbootCmds.c
+++ QcomModulePkg/Library/FastbootLib/FastbootCmds.c
@@ -97,6 +97,9 @@ STATIC CONST CHAR16 *CriticalPartitions[] = {
     L"abl",  L"rpm",        L"tz",      L"sdi",       L"xbl",       L"hyp",
     L"pmic", L"bootloader", L"devinfo", L"partition", L"devcfg",    L"ddr",
     L"frp",  L"cdt",        L"cmnlib",  L"cmnlib64",  L"keymaster", L"mdtp"};
+
+STATIC BOOLEAN
+IsCriticalPartition (CHAR16 *PartitionName);
 #endif
 
 STATIC FASTBOOT_VAR *Varlist;
@@ -1224,6 +1227,13 @@ HandleMetaImgFlash (IN CHAR16 *PartitionName,
       return EFI_INVALID_PARAMETER;
     }
     AsciiStrToUnicodeStr (img_header_entry[i].ptn_name, PartitionNameFromMeta);
+
+    if (!IsUnlockCritical () &&
+        IsCriticalPartition (PartitionNameFromMeta)) {
+      FastbootFail ("Flashing is not allowed for Critical Partitions\n");
+      return EFI_INVALID_PARAMETER;
+    }
+
     Status = HandleRawImgFlash (
         PartitionNameFromMeta, ARRAY_SIZE (PartitionNameFromMeta),
         (void *)Image + img_header_entry[i].start_offset,
